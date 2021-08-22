4adb2c8cdf9e8cb153747c183d589de12e8d9d93
Ranjit Singh <ranjitsingh@ranjits-mbp.home>
2021-08-22 14:25:56 +0100

    changed on feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from reactjs.
+          Merged from feature branch into master.
         </p>
         <a
           className="App-link"
