813cf2584f8fb40692771945466a12843d859648
Ranjit Singh <ranjitsingh@ranjits-mbp.home>
2021-08-23 15:54:18 +0100

    simple change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Merged from feature branch into master.
+          Sainsbury Demo.
         </p>
         <a
           className="App-link"
